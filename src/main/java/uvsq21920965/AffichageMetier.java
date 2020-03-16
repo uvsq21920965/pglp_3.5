e4dc1357ebf055edb30671074c217ca1473bf7e3
uvsq21920965 <sarra.belmahdi@gmail.com>
2020-03-16 19:52:36 +0100

    java doc 2

--- src/main/java/uvsq21920965/AffichageMetier.java
+++ src/main/java/uvsq21920965/AffichageMetier.java
@@ -3,19 +3,29 @@ package uvsq21920965;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * AffichageMetier classe.
+ * @author Sarrra Belmahdi.
+ *
+ */
 
 public class AffichageMetier implements Affichage{
-	Logger logger = LoggerFactory.getLogger(AffichageMetier.class);
-	public void AffichageDebut() {
-		 
-		    logger.info("Début  de  uneMethodeMetier" );
-	}
+/**
+ * initilise logger.
+ */
+private static final Logger logger = LoggerFactory.getLogger(AffichageMetier.class);
 
-	public void AffichageFin() {
-		 
-		    logger.info("fin  de  uneMethodeMetier" );
+  /**
+   * enregistrer un message INFO.
+   */
+  public void AffichageDebut() {
+    logger.info("Début  de  uneMethodeMetier" );
+  }
 
-	}
-	
-
+  /**
+   * enregistrer un message INFO.
+   */
+  public void AffichageFin() {
+    logger.info("fin  de  uneMethodeMetier" );
+  }
 }
